726b52a27a7f46b22c21d412a8d3336e4c3086d4
xushjie1987 <xsj2013@foxmail.com>
2016-05-10 00:13:22 +0800

    commit 5

--- src/main/java/com/oneapm/es/kafka/producer/PooledProducerCmd.java
+++ src/main/java/com/oneapm/es/kafka/producer/PooledProducerCmd.java
@@ -10,6 +10,10 @@
 package com.oneapm.es.kafka.producer;
 
 import io.airlift.airline.Command;
+import io.airlift.airline.Option;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * ClassName:PooledProducerCmd <br/>
@@ -25,12 +29,46 @@ import io.airlift.airline.Command;
          description = "线程池Kafka producer客户端")
 public class PooledProducerCmd extends ProducerCommand {
     
+    @Option(name = { "-f", "--fixed" },
+            required = false,
+            description = "fixed模式的线程池，默认关闭")
+    public Boolean          fixed      = false;
+    
+    @Option(name = { "-h", "--cached" },
+            required = false,
+            description = "cached模式的线程池，默认关闭")
+    public Boolean          cached     = false;
+    
+    @Option(name = { "-g", "--single" },
+            required = false,
+            description = "single模式的线程池，默认关闭")
+    public Boolean          single     = false;
+    
+    @Option(name = { "-s", "--scaled" },
+            required = false,
+            description = "scaled模式的线程池，默认关闭")
+    public Boolean          scaled     = false;
+    
+    private ExecutorService threadPool = null;
+    
     /**
      * @see java.lang.Runnable#run()
      */
     @Override
     public void run() {
         System.out.println("暂未开发");
+    }
+    
+    /**
+     * init: <br/>
+     * 
+     * @author xushjie
+     * @since JDK 1.7
+     */
+    private void init() {
+        if (fixed) {
+            threadPool = Executors.newFixedThreadPool(1);
+        }
     }
     
 }
